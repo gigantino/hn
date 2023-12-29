6afd914eeef557c304312baff1508825e206132c
gigantino <giovanni.gigantino@atoms.retexspa.com>
2023-12-29 23:22:21 +0100

    feat: add vite pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
   integrations: [tailwind(), icon()],
+  vite: {
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+      }),
+    ],
+  },
 });
