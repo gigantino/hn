f682aeb992846f7289e5e3a447f802b9b5f6b556
giovanni <git@gigantino.dev>
2025-03-27 11:58:03 +0100

    feat(next)!: move from astro to react

--- src/env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="astro/client" />
-interface ImportMetaEnv {
-  readonly HN_API: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
--- src/modules/getPage.ts
+++
@@ -1,8 +0,0 @@
-import type { AstroGlobal } from "astro";
-
-const getPage = (Astro: Readonly<AstroGlobal<Record<string, any>>>) => {
-  const rawPage = Number(Astro.url.searchParams.get("page"));
-  return isNaN(rawPage) || rawPage <= 1 ? 1 : rawPage;
-};
-
-export default getPage;
