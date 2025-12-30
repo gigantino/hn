b401ac245f90cb38861a96be96e228fc488767bd
gigantino <git@gigantino.dev>
2025-12-30 17:06:51 +0100

    feat: add prev and next functionality to comments

--- src/modules/getRelativeTime.ts
+++ src/modules/getRelativeTime.ts
@@ -1,22 +1,26 @@
-const timeUnits: { unit: Intl.RelativeTimeFormatUnit; duration: number }[] = [
-  { unit: "year", duration: 31536000000 },
-  { unit: "month", duration: 2628000000 },
-  { unit: "day", duration: 86400000 },
-  { unit: "hour", duration: 3600000 },
-  { unit: "minute", duration: 60000 },
-  { unit: "second", duration: 1000 },
+const timeUnits: { unit: Intl.RelativeTimeFormatUnit; short: string; duration: number }[] = [
+  { unit: "year", short: "y", duration: 31536000000 },
+  { unit: "month", short: "mo", duration: 2628000000 },
+  { unit: "day", short: "d", duration: 86400000 },
+  { unit: "hour", short: "h", duration: 3600000 },
+  { unit: "minute", short: "m", duration: 60000 },
+  { unit: "second", short: "s", duration: 1000 },
 ];
 
-const getRelativeTime = (timestamp: number) => {
+const getRelativeTime = (timestamp: number, short: boolean = false) => {
   const msElapsed = Date.now() - timestamp * 1000;
-  const intlRelativeTimeFormat = new Intl.RelativeTimeFormat("en", { style: "long" });
   const timeUnit = timeUnits.find(({ duration }) => msElapsed >= duration);
 
-  if (!timeUnit) return "just now";
+  if (!timeUnit) return short ? "now" : "just now";
 
-  const { unit, duration } = timeUnit;
+  const { unit, short: shortUnit, duration } = timeUnit;
   const timeElapsed = Math.floor(msElapsed / duration);
 
+  if (short) {
+    return `${timeElapsed}${shortUnit}`;
+  }
+
+  const intlRelativeTimeFormat = new Intl.RelativeTimeFormat("en", { style: "long" });
   return intlRelativeTimeFormat.format(-timeElapsed, unit);
 };
 
