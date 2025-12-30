2a3a45f90fc46c5e370ddcc861b1081272ad80f1
gigantino <git@gigantino.dev>
2025-12-30 17:39:52 +0100

    feat: improve search

--- src/modules/makeSearch.ts
+++ src/modules/makeSearch.ts
@@ -1,7 +1,13 @@
-const makeSearch = async (query: string) => {
-  const res = await fetch(`https://hn.algolia.com/api/v1/search?query=${query}`);
+const ITEMS_PER_PAGE = 20;
 
-  return (await res.json()).hits.map((d) => {
+const makeSearch = async (query: string, page: number = 1) => {
+  const res = await fetch(
+    `https://hn.algolia.com/api/v1/search?query=${encodeURIComponent(query)}&page=${page - 1}&hitsPerPage=${ITEMS_PER_PAGE}`
+  );
+
+  const data = await res.json();
+
+  const hits = data.hits.map((d) => {
     return {
       id: d.objectID,
       by: d.author,
@@ -12,6 +18,13 @@ const makeSearch = async (query: string) => {
       time: new Date(d.created_at).getTime() / 1000,
     };
   });
+
+  return {
+    hits,
+    totalResults: data.nbHits,
+    totalPages: data.nbPages,
+    currentPage: page,
+  };
 };
 
 export default makeSearch;
